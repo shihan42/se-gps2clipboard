ba7e8c929a62568d2f5c6b038bf94daef24a3449
Shihan <shihan42@protonmail.com>
2025-07-28 12:12:05 +0200

    Basic plugin code added

---
+++ ClientPlugin/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using VRage.Plugins;
+
+namespace Gps2Clipboard
+{
+    // ReSharper disable once UnusedType.Global
+    // ReSharper disable once UnusedMember.Global
+    public class Plugin : IPlugin
+    {
+        private const string Name = "Gps2Clipboard";
+
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public void Init(object gameInstance)
+        {
+            var harmony = new Harmony(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Dispose()
+        {}
+
+        public void Update()
+        {}
+    }
+}
